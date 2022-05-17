e96511d06351c683cd535ea9b306fe733369b78e
Aidan Denham <105383688+0fficialCheesecakeFactory@users.noreply.github.com>
2022-05-17 14:04:15 -0400

    Day 1

--- Checkers.java
+++ Checkers.java
@@ -1,4 +1,31 @@
+import java.io.File;
 
-public class Checkers {
-
+public class Checkers 
+{
+	private CheckersBoard board;
+	private int firstToAct;
+	
+	public Checkers() 
+	{
+		board = new CheckersBoard();
+		firstToAct = 1;
+	}
+	
+	public Checkers(File startingPositions, int firstToAct)
+	{
+		this.firstToAct = firstToAct;
+		board = new CheckersBoard(startingPositions);
+	}
+	//to be implemented later
+	public void run() 
+	{
+		
+	}
+	
+	public CheckersBoard boardResult(File moves)
+	{
+		
+		CheckersBoard board = new CheckersBoard(moves);
+		return board;
+	}
 }
