e85de44f461241bf3187612569d4ba051a140b36
Aidan Denham <105383688+0fficialCheesecakeFactory@users.noreply.github.com>
2022-05-18 14:03:47 -0400

    Day 1.5
    Needs rules to be finished

--- Driver.java
+++ Driver.java
@@ -1,9 +1,27 @@
-
+import java.util.Scanner;
 public class Driver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+	    Scanner scan = new Scanner(System.in);
+	    String lcv;
+		System.out.println("Would you like to play checkers?");
+		lcv = scan.next();
+        while(lcv.equals("Y"))
+        {
+            printRules();
+            Checkers game = new Checkers();
+            game.run();
+            System.out.println("Would you like to play again?");
+            lcv = scan.next();
+        }
+	}
+	public void printRules() 
+	{
+	    System.out.println("Checkers is a game involving two people.");
+	    System.out.println("Each player will take their turn and each turn is as follows:");
+	    System.out.println("at the start of each turn you will recieve a list of all your potential moves");
+	    System.out.println("you will then ")
 	}
 
 }
