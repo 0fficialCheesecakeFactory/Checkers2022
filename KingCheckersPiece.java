7d7444d00daa3485abb25356b065a945b6c7c155
Aidan Denham <105383688+0fficialCheesecakeFactory@users.noreply.github.com>
2022-05-19 12:54:16 -0400

    Day 2

--- KingCheckersPiece.java
+++ KingCheckersPiece.java
@@ -1,4 +1,28 @@
+import java.util.*;
 
-public class KingCheckersPiece {
+public class KingCheckersPiece extends CheckersPiece
+{
+	public KingCheckersPiece(int priority, int row, int col, CheckersBoard board)
+	{
+		super(priority, row, col, board);
+	}
 
+	public ArrayList<String> legalMoves()
+	{
+		//Copy from parent when it is complete
+		//combine validity for r and b
+	}
+
+	public boolean canJump()
+	{
+		//Copy from parent when it is complete
+		//combine validity for r and b
+	}
+
+	public String toString()
+	{
+		if(priority == 0) return " ";
+		else if(priority == 1) return "B";
+		else return "R";
+	}
 }
