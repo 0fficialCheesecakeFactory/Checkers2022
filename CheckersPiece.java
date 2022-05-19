cb8749a96c207c06879e8e5b5ef565a9fb5cf7ea
Aidan Denham <105383688+0fficialCheesecakeFactory@users.noreply.github.com>
2022-05-19 12:53:58 -0400

    Day 2

--- CheckersPiece.java
+++ CheckersPiece.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 public class CheckersPiece 
 {
-	private int priority;
-	private int row;
-	private int column;
-	private CheckersBoard myBoard;
+	protected int priority;
+	protected int row;
+	protected int column;
+	protected CheckersBoard myBoard;
 
 	public CheckersPiece(int priority, int row, int col, CheckersBoard board)
 	{
@@ -17,38 +17,42 @@ public class CheckersPiece
 	public ArrayList<String> legalMoves() 
 	{
 		ArrayList<String> moves = new ArrayList<String>();
-		if(canJump()) //needs implementation
+		String ret = rowToString() + column;
+		int yoffset, doubleoffset;
+		if(priority == 2)
+			yoffset = 1;
+		else yoffset = -1;
+		doubleoffset = yoffset*2;
+		if(canJump()) //TODO Aidan: Need to change list of moves so that it only does the list of jumps
 		{
-
+			
 		}
 		else try 
 		{
-			String ret = rowToString() + column;
-			int xoffset;
-			if(priority == 2)
-				xoffset = 1;
-			else xoffset = -1;
-			if(myBoard.getPiece(row+xoffset, column-1) == null && priority == 1)
-			{
-				ret += myBoard.getPiece().rowToString() + myBoard.getPiece.getColumn();
-				moves.add(ret);
-			}
 			
-			if(myBoard.getPiece(row+xoffset, column-1) == null && priority == 1)
+			if(myBoard.getPiece(row-1, column+yoffset) == null && priority == 1)
 			{
-				ret += myBoard.getPiece().rowToString() + myBoard.getPiece.getColumn();
+				ret += myBoard.getPiece(row-1, column+yoffset).rowToString() + (column-1);
 				moves.add(ret);
 			}
+
+			if(myBoard.getPiece(row-1, column+yoffset) == null && priority == 1)
+			{
+				ret += myBoard.getPiece(row-1, column-1+yoffset).rowToString() + (column-1);
+				moves.add(ret);
+			}
+
 		}catch(IndexOutOfBoundsException e)
 		{
-			
+
 		}
+		return moves;
 	}
-	
+
 	public boolean move(int row, int col)
 	{
 		ArrayList<String> legal = legalMoves();
-		String move = rowToString() + column + myBoard.getPiece(row, col).rowToString() + myBoard.getPiece(row, col).getColumn();
+		String move = rowToString() + column + myBoard.getPiece(row, col).rowToString() + myBoard.getPiece(row, col).column;
 		if(!legal.contains(move))
 			return false;
 		else
@@ -58,5 +62,66 @@ public class CheckersPiece
 		}
 	}
 
+	public void swap(CheckersPiece piece, int row, int col) //first piece is being swapped/jumped with second piece
+	{
+		CheckersPiece temp = piece;
+		if(myBoard.getPiece(row, col) == null)
+		{
+			piece.row = row;
+			piece.column = col;
 
+			row = temp.row;
+			col = temp.column;
+		}
+	}
+
+	public boolean canJump() 
+	{
+		int yoffset = 0, doubleoffset = 0;
+		if(priority == 1) yoffset = 1;
+		else yoffset = -1;
+		doubleoffset = yoffset *2;
+		try 
+		{
+			if((myBoard.getPiece(row+1, column+yoffset).getPriority() == 2) && (myBoard.getPiece(row+2, column+doubleoffset).equals(null)))
+			{
+				return true;
+			}
+			else if((myBoard.getPiece(row-1, column+yoffset).getPriority() == 2) && (myBoard.getPiece(row-2, column+doubleoffset).equals(null)))
+			{
+				return true;
+			}
+		}catch(IndexOutOfBoundsException e)
+		{
+
+		}
+		return false;
+	}
+
+	public String rowToString() 
+	{
+		return String.valueOf((char) (65+row));
+	}
+
+	public String toString() 
+	{
+		if(priority == 0) return " ";
+		else if(priority == 1) return "b";
+		else return "r";
+	}
+
+	public int getPriority()
+	{
+		return priority;
+	}
+
+	public int getRow()
+	{
+		return row;
+	}
+
+	public int getCol()
+	{
+		return column;
+	}
 }
