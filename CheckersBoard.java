d1f356e52bc222e8ea55b6d298814439a4b3709c
Aidan Denham <105383688+0fficialCheesecakeFactory@users.noreply.github.com>
2022-05-19 12:54:37 -0400

    Day 2

--- CheckersBoard.java
+++ CheckersBoard.java
@@ -49,18 +49,38 @@ public class CheckersBoard
 		flip(checkersBoard);
 	}
 
-//TODO Mantis: Finish this; copy each character of the file into a char[][], then use the position of each char in that array to place them in the board.
+	//copy each character of the file into a char[][], then use the position of each char in that array to place them in the board.
 	public CheckersBoard(File f)
 	{
-		Scanner scan = new Scanner(System.in);
 		char[][] spots = new char[8][8];
 		try
 		{
+			Scanner scan = new Scanner(f);
 			for(int i = 0; i < f.length(); i++)
 			{
+				String temp = scan.next();
 				for(int c = 0; c < spots.length; c++)
 				{
-					char[i][c] = 
+					spots[i][c] = temp.charAt(c); //translates the file into a 2d array
+				}
+			}
+
+			for (int r = 0; r < spots.length; r++)
+			{
+				for (int col = 0; col < spots[r].length; col++) //translates the characters in the array to actual pieces
+				{
+					if (spots[r][col] == 'r') //red
+					{
+						checkersBoard[r][col] = new CheckersPiece(2, r, col, this);
+					}
+					else if (spots[r][col] == 'b') //black
+					{
+						checkersBoard[r][col] = new CheckersPiece(1, r, col, this);
+					}
+					else //null pieces (spaces)
+					{
+						checkersBoard[r][col] = null;
+					}
 				}
 			}
 		}
@@ -82,6 +102,46 @@ public class CheckersBoard
 			}
 		}
 		return flipBoard;
+	}
+
+	public CheckersPiece getPiece(int row, int col)
+	{
+		return checkersBoard[row][col];
+	}
+
+	public KingCheckersPiece kingPiece(int row, int col)
+	{
+		KingCheckersPiece king = new KingCheckersPiece(checkersBoard[row][col].getPriority(), row, col, this);
+		checkersBoard[row][col] = king;
+		return king;
+	}
+
+	public int[] getPiecePosition(CheckersPiece piece)
+	{
+		int[] pos = new int[2];
+		pos[0] = piece.getRow();
+		pos[1] = piece.getCol();
+		return pos;
+	}
+
+	public String toString()
+	{
+		String out = "";
+		for(int r = 0; r < checkersBoard.length; r++)
+		{
+			for (int c = 0; c < checkersBoard[r].length; c++)
+			{
+				out += checkersBoard[r][c];
+			}
+			out += "\n";
+		}
+
+		return out;
+	}
+
+	public void printBoard()
+	{
+		//figure out an algorithm to prin a visual representation of the board
 	}
 
 }
