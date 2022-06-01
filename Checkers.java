c5dbe8172520559ad0a78fa6b735064a86d09a5a
Aidan Denham <105383688+0fficialCheesecakeFactory@users.noreply.github.com>
2022-06-01 14:02:32 -0400

    Day 3

--- Checkers.java
+++ Checkers.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 
 public class Checkers 
 {
@@ -19,7 +20,32 @@ public class Checkers
 	//to be implemented later
 	public void run() 
 	{
+		board.printBoard(); //for testing purposes
+		try 
+		{
+			ArrayList<String> jump = new ArrayList<String>();
+			ArrayList<String> noJump = new ArrayList<String>();
+			for(int i = 0; i < 8; i++)
+			{
+				for(int j = 0; j < 8; j++)
+					{
+						if(board.getPiece(i, j).getPriority() == 1)
+						{
+							if(board.getPiece(i, j).canJump())
+							jump.addAll(board.getPiece(i, j).legalMoves());	
+							else noJump.addAll(board.getPiece(i, j).legalMoves());
+						}
+					}
+			}
+			if(jump.isEmpty())
+				System.out.println(noJump);
+			else System.out.println(jump);
 
+		}
+		catch(Exception e)
+		{
+			
+		}
 	}
 
 	public CheckersBoard boardResult(File moves)
