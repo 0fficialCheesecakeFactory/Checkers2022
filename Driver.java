be777343ecac7d234978a7fabcf4babc28fa58e6
Aidan Denham <105383688+0fficialCheesecakeFactory@users.noreply.github.com>
2022-06-09 15:33:35 -0400

    Day 4

--- Driver.java
+++ Driver.java
@@ -6,41 +6,32 @@ public class Driver {
 	public static void main(String[] args)
 	{
 		
-//		Scanner scan = new Scanner(System.in);
-//		String lcv;
-//		File f = new File("testBoard.txt");
-//		System.out.println("Would you like to play checkers? (y/n)");
-//		lcv = scan.next();
-//		while(lcv.equalsIgnoreCase("Y"))
-//		{
-//			printRules();
-//			Checkers game = new Checkers(f,1);
-//			game.run();
-//			System.out.println("Would you like to play again?");
-//			lcv = scan.next();
-//		}
+		Scanner scan = new Scanner(System.in);
+		String lcv;
+		File f = new File("testBoard.txt");
+		System.out.println("Would you like to play checkers? (y/n)");
+		lcv = scan.next();
+		while(lcv.equalsIgnoreCase("Y"))
+		{
+			printRules();
+			Checkers game = new Checkers(f, 2);
+			game.run();
+			System.out.println("Would you like to play again?");
+			lcv = scan.next();
+		}
 		
-		CheckersBoard board = new CheckersBoard(new File("testBoard.txt"));
-		board.kingPiece(1, 2);
-		board.printBoard();
-		System.out.println(board.getPiece(1, 2));
 		
-		board.getPiece(1, 2).move(2, 1);
-		System.out.println(board.getPiece(2, 1));
-		board.printBoard(); //king pieces are not moved correctly
-//		board.getPiece(0, 3).move(1, 2);
-//		board.printBoard();
-		//System.out.println(board.getPiece(2, 1).legalMoves());
 	
 	}
 	public static void printRules() 
 	{
-System.out.println					 	  	 ("Checkers is a game involving two people.");
-System.out.println	("Each player will take their turn and each turn is as follows:");
-System.out.println("at the start of each turn you will recieve a list of all your potential moves");
-System.out.println		  				   ("you will then jump if you can jump");
-System.out.println					  ("Or you will move a piece forward in a diagnal by 1 square");
-System.out.println											("Do not move off board");
-System.out.println			   ("if piece Kinged then it can move backward in a diagnal");
+System.out.println("Checkers is a game involving two people.");
+System.out.println("Each player will take their turn and each turn is as follows:");
+System.out.println("At the start of each turn you will recieve a list of all your potential moves");
+System.out.println("You will then jump if you can jump");
+System.out.println("Or you will move a piece forward in a diagonal by 1 square");
+System.out.println("Do not move off board");
+System.out.println("If a piece is Kinged then it can move backwards in a diagonal");
+System.out.println("To win, you must eliminate all of the other player's pieces'");
 	}
 }
