12405a7026c8e0a6a307f54f7455dd3d96de5204
Aidan Denham <105383688+0fficialCheesecakeFactory@users.noreply.github.com>
2022-06-10 15:00:27 -0400

    Day 5
    Done?

--- Driver.java
+++ Driver.java
@@ -1,37 +1,41 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Driver {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		
+
 		Scanner scan = new Scanner(System.in);
 		String lcv;
+		//File f = new File("testBoard.txt");
 		File f = new File("testBoard.txt");
 		System.out.println("Would you like to play checkers? (y/n)");
 		lcv = scan.next();
 		while(lcv.equalsIgnoreCase("Y"))
 		{
 			printRules();
-			Checkers game = new Checkers(f, 2);
+			Checkers game = new Checkers();
+
 			game.run();
 			System.out.println("Would you like to play again?");
 			lcv = scan.next();
+
 		}
-		
-		
-	
+
+
+
 	}
 	public static void printRules() 
 	{
-System.out.println("Checkers is a game involving two people.");
-System.out.println("Each player will take their turn and each turn is as follows:");
-System.out.println("At the start of each turn you will recieve a list of all your potential moves");
-System.out.println("You will then jump if you can jump");
-System.out.println("Or you will move a piece forward in a diagonal by 1 square");
-System.out.println("Do not move off board");
-System.out.println("If a piece is Kinged then it can move backwards in a diagonal");
-System.out.println("To win, you must eliminate all of the other player's pieces'");
+		System.out.println("Checkers is a game involving two people.");
+		System.out.println("Each player will take their turn and each turn is as follows:");
+		System.out.println("At the start of each turn you will recieve a list of all your potential moves");
+		System.out.println("You will then jump if you can jump");
+		System.out.println("Or you will move a piece forward in a diagonal by 1 square");
+		System.out.println("Do not move off board");
+		System.out.println("If a piece is Kinged then it can move backwards in a diagonal");
+		System.out.println("To win, you must eliminate all of the other player's pieces'");
 	}
 }
